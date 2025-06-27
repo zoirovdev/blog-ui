e04b5acfcce2165ec644417cfe5440a6d30612da
zoirovdev <abboszoirov17@gmail.com>
2025-06-28 04:09:22 +0500

    update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline"
+
 
 
 const Home = () => {
@@ -61,21 +63,27 @@ const Home = () => {
           ) : (
             posts.map(post => (
               <article key={post.id} className="border-b border-slate-400 pb-4">
-                <Link 
-                  to={`/view/${post.id}`}
-                  className="block hover:bg-gray-50 p-2 -m-2 rounded transition-colors"
-                >
-                  <h2 className="text-xl font-semibold mb-2 text-blue-600 hover:text-blue-800">
-                    {post.title}
-                  </h2>
+                <div className="block hover:bg-gray-50 p-2 -m-2 rounded transition-colors space-y-4">
+		  <div className="flex flex-row justify-between items-center">
+                    <h2 className="text-xl font-semibold mb-2 text-blue-600 hover:text-blue-800">
+                      {post.title}
+                    </h2>
+		    <Link to={`/view/${post.id}`} 
+		      className="ml-2 border border-slate-400 hover:bg-white hover:shadow-md rounded-[50%]
+			p-[5px]">
+		      <ArrowTopRightOnSquareIcon className="w-5 h-5"/>
+		    </Link>
+		  </div>
                   <p className="text-gray-800 mb-2 line-clamp-3">
                     {post.content}
                   </p>
                   <div className="text-sm text-gray-600">
-                    By {post.author?.firstName} {post.author?.lastName} • 
-                    {new Date(post.createdAt).toLocaleDateString()}
+                    <div className="flex flex-row justify-between items-center">
+		      <p>@{post.author?.username}</p> 
+                      <p>{new Date(post.createdAt).toLocaleDateString()}</p>
+		    </div>
                   </div>
-                </Link>
+                </div>
               </article>
             ))
           )}
