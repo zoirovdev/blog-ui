472ffb5b94a60c0ad5b2540edb10b3c30165da64
zoirovdev <abboszoirov17@gmail.com>
2025-06-28 09:20:54 +0500

    update SearchResult.jsx

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useLocation, Link } from 'react-router-dom'
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline'
 
 
 
@@ -24,21 +25,25 @@ const SearchResult = () => {
           ) : (
             data.results.map(post => (
               <article key={post.id} className="border-b border-slate-400 pb-4">
-                <Link 
-                  to={`/view/${post.id}`}
-                  className="block hover:bg-gray-50 p-2 -m-2 rounded transition-colors"
-                >
-                  <h2 className="text-xl font-semibold mb-2 text-blue-600 hover:text-blue-800">
-                    {post.title}
-                  </h2>
-                  <p className="text-gray-800 mb-2 line-clamp-3">
+                <div className="block hover:bg-gray-50 p-2 -m-2 rounded transition-colors space-y-4">
+		  <div className="flex flex-row justify-between items-center">
+                    <h2 className="text-xl font-semibold mb-2 text-blue-600 hover:text-blue-800">
+                      {post.title}
+                    </h2>
+		    <Link to={`/view/${post.id}`} 
+		      className="border border-slate-400 p-[5px] rounded-[50%]
+			hover:bg-white hover:shadow-md">
+		      <ArrowTopRightOnSquareIcon className="w-5 h-5"/>
+		    </Link>
+		  </div>
+                  <p className="text-gray-800 mb-2 line-clamp-4">
                     {post.content}
                   </p>
-                  <div className="text-sm text-gray-600">
-                    By {post.author?.firstName} {post.author?.lastName} • 
-                    {new Date(post.createdAt).toLocaleDateString()}
+                  <div className="text-sm text-gray-600 flex flex-row justify-between items-center">
+		    <p>@{post.author?.username}</p> 
+                    <p>{new Date(post.createdAt).toLocaleDateString()}</p>
                   </div>
-                </Link>
+                </div>
               </article>
             ))
           )}
